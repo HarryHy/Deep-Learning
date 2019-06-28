d15db5993e5980c52686f30f655b980db0e0ed2d
Harry <30531325+HarryHy@users.noreply.github.com>
2019-06-28 15:24:42 -0400

    Update narrow.py

--- Net2Net/narrow.py
+++ Net2Net/narrow.py
@@ -4,21 +4,29 @@ x = torch.Tensor([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],
 print(x.shape)
 # torch.Size([2, 3, 4])
 x.narrow(0, 1, 1)
+
 '''
 tensor([[[ 21.,  22.,  23.,  24.],
          [ 25.,  26.,  27.,  28.],
          [ 29., 210., 211., 212.]]])
 '''
+
 x = torch.Tensor([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
 x.narrow(0, 0, 2)
+
 '''
 tensor([[1., 2., 3., 4.],
         [5., 6., 7., 8.]])
 '''
 
 x.narrow(1, 0, 2)
+
 '''
 tensor([[ 1.,  2.],
         [ 5.,  6.],
         [ 9., 10.]])
 '''
+#torch.Tensor.narrow(dimension, start, length) → Tensor
+#dimension (int) – 要进行缩小的维度
+#start (int) – 开始维度索引
+#length (int) – 缩小持续的长度
